3521fd1d23f6d57db173438e4c92b6aa6062fe08
Marco Cruz <marco24cruz08@gmail.com>
2024-12-31 20:44:30 -0600

    Añadir infraestructura y servicios para Kafka y MySQL
    Se ha añadido un nuevo proyecto `Banking.Account.Query.Infrastructure` al archivo de solución `NetApache.sln` y se ha referenciado en el archivo `Banking.Account.Query.Application.csproj`.
    
    Se ha eliminado la carpeta `Models` del proyecto `Banking.Account.Query.Application`.
    
    Se ha añadido una nueva clase `KafkaSettings` en el espacio de nombres `Banking.Account.Query.Application.Models` para gestionar la configuración de Kafka.
    
    Se ha creado el archivo de proyecto `Banking.Account.Query.Infrastructure.csproj` con referencias a varios paquetes NuGet, incluyendo `Confluent.Kafka`, `Microsoft.EntityFrameworkCore`, `Newtonsoft.Json`, entre otros.
    
    Se ha añadido una clase `InfrastructureServiceRegistration` para registrar servicios de infraestructura en el contenedor de dependencias.
    
    Se ha añadido un servicio `BankAccountConsumerService` que implementa `IHostedService` para consumir eventos de Kafka y procesarlos.
    
    Se ha añadido una clase `MySqlDbContext` que hereda de `DbContext` para gestionar la conexión a la base de datos MySQL.
    
    Se ha añadido un repositorio `BankAccountRepository` que implementa `IBankAccountRepository` para gestionar las operaciones CRUD de cuentas bancarias.
    
    Se ha añadido una clase base `BaseRepository` que implementa `IAsyncRepository<T>` para operaciones genéricas de repositorio.

---
+++ Banking.Account.Query.Infrastructure/Repositories/BankAccountRepository.cs
@@ -0,0 +1,88 @@
+﻿using Banking.Account.Query.Application.Contracts.Persistence;
+using Banking.Account.Query.Domain;
+using Banking.Account.Query.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Banking.Account.Query.Infrastructure.Repositories
+{
+    public class BankAccountRepository : BaseRepository<BankAccount>, IBankAccountRepository
+    {
+        public BankAccountRepository(MySqlDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task DeleteByIdentifier(string identifier)
+        {
+            var bankAccount = await _dbContext.BankAccounts!.Where(x => x.Identifier == identifier).FirstOrDefaultAsync();
+            if (bankAccount is null)
+            {
+                throw new Exception($"Cannot delete with id {identifier}");
+            }
+            _dbContext.BankAccounts!.Remove(bankAccount);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DepositBankAccountByIdentifier(BankAccount bankAccount)
+        {
+            var account = await _dbContext.BankAccounts!
+                .Where(x => x.Identifier == bankAccount.Identifier)
+                .FirstOrDefaultAsync();
+
+            if (account is null)
+            {
+                throw new Exception($"Can not update with id {bankAccount.Identifier}");
+            }
+
+            account.Balance += bankAccount.Balance;
+            await UpdateAsync(account);
+        }
+
+        public async Task<IEnumerable<BankAccount>> FindByAccountHolder(string accountHolder)
+        {
+            return await _dbContext.BankAccounts!
+                .Where(x => x.AccountHolder == accountHolder)
+                .ToListAsync();
+        }
+
+        public async Task<BankAccount> FindByAccountIdentifier(string identifier)
+        {
+            return (await _dbContext.BankAccounts!
+                .Where(x => x.Identifier == identifier)
+                .FirstOrDefaultAsync())!;
+        }
+
+        public async Task<IEnumerable<BankAccount>> FindByBalanceGreaterThan(double balance)
+        {
+            return await _dbContext.BankAccounts!
+                           .Where(x => x.Balance > balance)
+                           .ToListAsync();
+        }
+
+        public async Task<IEnumerable<BankAccount>> FindByBalanceLessThan(double balance)
+        {
+            return await _dbContext.BankAccounts!
+                           .Where(x => x.Balance < balance)
+                           .ToListAsync();
+        }
+
+        public async Task WithdrawnBankAccountByIdentifier(BankAccount bankAccount)
+        {
+            var account = await _dbContext.BankAccounts!
+                .Where(x => x.Identifier == bankAccount.Identifier)
+                .FirstOrDefaultAsync();
+
+            if (account is null)
+            {
+                throw new Exception($"Can not update with id {bankAccount.Identifier}");
+            }
+
+            if (account.Balance < bankAccount.Balance)
+            {
+                throw new Exception($"Not enough money");
+            }
+
+            account.Balance -= bankAccount.Balance;
+            await UpdateAsync( account );
+        }
+    }
+}
---
+++ Banking.Account.Query.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,64 @@
+﻿using Banking.Account.Query.Application.Contracts.Persistence;
+using Banking.Account.Query.Domain;
+using Banking.Account.Query.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Banking.Account.Query.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IAsyncRepository<T> where T : BaseDomainModel
+    {
+        protected readonly MySqlDbContext _dbContext;
+
+        public BaseRepository(MySqlDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public void AddEntity(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public void DeleteEntity(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return (await _dbContext.Set<T>().FindAsync(id))!;
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _dbContext.Set<T>().Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public void UpdateEntity(T entity)
+        {
+            _dbContext.Set<T>().Attach(entity);
+            _dbContext.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
